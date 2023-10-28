d3ddc87bec9f48d26c80703a9e289b179641c72f
shikhar360 <sssonishikhar360@gmail.com>
2023-10-28 15:25:05 +0530

    changes after merge

--- src/app/_component/Footer.jsx
+++ src/app/_component/Footer.jsx
@@ -6,14 +6,14 @@ export default function Footer() {
 <footer className="relative">
 <div className="absolute inset-0 -z-10 bg-tertiary-color"></div>
 
-  <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8 relative z-10">
-    <div class="sm:flex sm:items-center sm:justify-between ">
-      <div class="flex justify-center items-center sm:justify-start">
+  <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8 relative z-10">
+    <div className="sm:flex sm:items-center sm:justify-between ">
+      <div className="flex justify-center items-center sm:justify-start">
       <Image src="/images/analytics.png" alt="logo" width={35} height={35}/>
       <h1 className="text-xl text-white font-semibold ml-3">Portolyzer</h1>
       </div>
 
-      <p class="mt-4 text-center text-sm text-white lg:mt-0 lg:text-right">
+      <p className="mt-4 text-center text-sm text-white lg:mt-0 lg:text-right">
         Copyright &copy; 2022. All rights reserved.
       </p>
     </div>
