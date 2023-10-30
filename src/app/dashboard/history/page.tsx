a5d359d15f8da1df517f1467c33c36fe16ce69f5
Harsh Singh <harshrajeevsingh@gmail.com>
2023-10-30 17:59:57 +0530

    added trx history

--- src/app/dashboard/history/page.tsx
+++ src/app/dashboard/history/page.tsx
@@ -2,12 +2,21 @@
 import { useStore } from '@/app/store/Store'
 import React, { useEffect, useState } from 'react'
 import { ToastContainer, toast , Flip } from "react-toastify";
+import TrxHistoryTable from '@/app/_component/TrxHistoryTable';
+type Transaction = {
+  transaction_hash: string;
+  block_number: number;
+  block_timestamp: string;
+  from_address: string;
+  to_address: string;
+  value: string
+};
 const History = () => {
   const eth = useStore(state => state.ethAddr)
   const chain = useStore(state => state.chain)
 
-  const [tx , setTx] = useState<any>('')
-
+  const [tx, setTx] = useState<Transaction[]>([]);
+  // const [toAddress, setToAddress] = useState<string>('');
   useEffect(()=>{
     if(!eth || !chain){
      toast.error("Please add your Wallet Address and Chain")
@@ -37,8 +46,11 @@ const History = () => {
           if(data){
   
             setTx(data)
+            if (data.length > 0) {
+              // setToAddress(data[0].to_address);
+            }
           }
-          console.log(data);
+          // console.log(data);
         } catch (err) {
           console.log(err);
           return ""
@@ -48,10 +60,47 @@ const History = () => {
 
     
   },[chain, eth])
-
+  
   return (
-    <div className={` pt-32`}>
-    <div>History</div>
+    <div className={` pt-28 pl-10`}>
+      <h1 className='text-2xl text-gray-800 font-bold lg:text-4xl mb-5'>Transaction History</h1>
+      {/* {toAddress && <h3 className='text-base sm:text-lg font-semibold text-gray-800'>To Address: {toAddress}</h3>} */}
+    <div>
+
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <div className="max-h-screen overflow-y-auto custom-scrollbar">
+    <table className="w-full text-sm text-left ">
+        <thead className="text-xs text-gray-800 uppercase bg-gray-300 sticky top-0">
+            <tr>
+                <th scope="col" className="px-6 py-3">
+                    Transaction Hash
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Block
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Date
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    From
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    To
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Value
+                </th>
+            </tr>
+        </thead>
+        {tx.map((transaction, index) => (
+          <TrxHistoryTable key={index} transaction={transaction} />
+        ))}
+        
+    </table>
+    </div>
+</div>
+        
+      </div>
     <ToastContainer
     position="bottom-right"
     autoClose={3000}
